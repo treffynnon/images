f38459bdc179649191c1fb341d15465c65ed976d
Simon Holywell <simon@emosaic.co.uk>
2015-07-29 10:51:42 +0100

    gnu parallel all the things

--- resize.sh
+++ resize.sh
@@ -6,17 +6,19 @@ TH_WIDTH=720
 TH_HEIGHT=70
 
 echo "Resizing in post images"
-
 FILES=$(find src -iname '*.jpg' -or -iname '*.png' -or -iname '*.gif')
-for file in $FILES
-do
-    echo "Resizing: $file"
-    FILENAME=$(basename "$file" | cut -d. -f1)
-    convert "$file" -strip -resize "${LG_WIDTH}x${LG_HEIGHT}^" -gravity center -crop "${LG_WIDTH}x${LG_HEIGHT}+0+0" -background black -flatten -filter catrom "t_post/${FILENAME}.jpg"
 
-    convert "t_post/${FILENAME}.jpg" -gravity center -crop "${TH_WIDTH}x${TH_HEIGHT}+0+0" -flatten -filter catrom -extent "${TH_WIDTH}x${TH_HEIGHT}" +repage "t_list/${FILENAME}.jpg"
+# process the large images
+parallel -j8 convert {} -strip -resize "${LG_WIDTH}x${LG_HEIGHT}^" -gravity center -crop "${LG_WIDTH}x${LG_HEIGHT}+0+0" -background black -flatten -filter catrom "t_post/{/.}.jpg" ::: $FILES
 
-    jpeg-recompress --method smallfry --accurate --quality high --min 60 "t_post/${FILENAME}.jpg" "t_post/${FILENAME}.jpg"
-    jpeg-recompress --method smallfry --quality medium --min 60 "t_post/${FILENAME}.jpg" "t_post/${FILENAME}.jpg"
-done
-echo "Resizing complete"
+# process the image slices
+parallel -j8 convert "t_post/{/.}.jpg" -gravity center -crop "${TH_WIDTH}x${TH_HEIGHT}+0+0" -flatten -filter catrom -extent "${TH_WIDTH}x${TH_HEIGHT}" +repage "t_list/{/.}.jpg" ::: $FILES
+
+# compress the large images
+parallel -j8 jpeg-recompress --method smallfry --quality medium --min 60 "t_post/{/.}.jpg" "t_post/{/.}.jpg" ::: $FILES
+
+# compress the image slices
+parallel -j8 jpeg-recompress --method smallfry --quality low --min 50 "t_list/{/.}.jpg" "t_list/{/.}.jpg" ::: $FILES
+
+echo " "
+echo "Completed resize operation"
