014a6e3a1a92a750fdee08774e88bbcb84cedf7a
Simon Holywell <simon@emosaic.co.uk>
2015-07-30 10:49:09 +0100

    Add in check to prevent processing images repeatedly

--- resize.sh
+++ resize.sh
@@ -5,24 +5,40 @@ LG_HEIGHT=480
 TH_WIDTH=720
 TH_HEIGHT=70
 
+TOUCHFILE="./simonholywell-images.lastrun.time"
+
+if [ ! -f "$TOUCHFILE" ]; then
+    touch -t 201507201231.42 "$TOUCHFILE"
+fi
+
 echo "Resizing in post images"
-FILES=$(find src -iname '*.jpg' -or -iname '*.png' -or -iname '*.gif')
+FILES=$(find src -newer "$TOUCHFILE" -iname '*.jpg' -or -newer "$TOUCHFILE" -iname '*.png' -or -newer "$TOUCHFILE" -iname '*.gif')
+echo "$FILES"
+if [ -n "$FILES" ]; then
+    # process the large images
+    parallel -j8 convert "{}" -strip -resize "${LG_WIDTH}x${LG_HEIGHT}^" -gravity center -crop "${LG_WIDTH}x${LG_HEIGHT}+0+0" -filter catrom "t_post/{/}" ::: $FILES
 
-# process the large images
-parallel -j8 convert {} -strip -resize "${LG_WIDTH}x${LG_HEIGHT}^" -gravity center -crop "${LG_WIDTH}x${LG_HEIGHT}+0+0" -filter catrom "t_post/{/}" ::: $FILES
-
-# process the image slices
-parallel -j8 convert "t_post/{/}" -gravity center -crop "${TH_WIDTH}x${TH_HEIGHT}+0+0" -filter catrom -extent "${TH_WIDTH}x${TH_HEIGHT}" +repage "t_list/{/}" ::: $FILES
+    # process the image slices
+    parallel -j8 convert "t_post/{/}" -gravity center -crop "${TH_WIDTH}x${TH_HEIGHT}+0+0" -filter catrom -extent "${TH_WIDTH}x${TH_HEIGHT}" +repage "t_list/{/}" ::: $FILES
+fi
 
 # compress jpg images
-parallel -j8 jpeg-recompress --method smallfry --quality medium --min 60 "{}" "{}" ::: t_post/*.jpg
-parallel -j8 jpeg-recompress --method smallfry --quality low --min 50 "{}" "{}" ::: t_list/*.jpg
+JPOST_FILES=$(find t_post -newer "$TOUCHFILE" -iname '*.jpg')
+JLIST_FILES=$(find t_list -newer "$TOUCHFILE" -iname '*.jpg')
+if [ -n "$JPOST_FILES" ]; then
+    parallel -j8 jpeg-recompress --method smallfry --quality medium --min 60 "{}" "{}" ::: $JPOST_FILES
+fi
+if [ -n "$JLIST_FILES" ]; then
+    parallel -j8 jpeg-recompress --method smallfry --quality low --min 50 "{}" "{}" ::: $JLIST_FILES
+fi
 
 # compress png images
-parallel -j8 optipng -o 3 -fix -preserve "{}" -out "{}" ::: t_post/*.png
-parallel -j8 optipng -o 3 -fix -preserve "{}" -out "{}" ::: t_list/*.png
-parallel -j8 advdef --shrink-extra -z "{}" ::: t_post/*.png
-parallel -j8 advdef --shrink-extra -z "{}" ::: t_list/*.png
+PNG_FILES=$(find t_post t_list -newer "$TOUCHFILE" -iname '*.png')
+if [ -n "$PNG_FILES" ]; then
+    parallel -j8 optipng -o 3 -fix "{}" -out "{}" ::: $PNG_FILES
+    parallel -j8 advdef --shrink-extra -z "{}" ::: $PNG_FILES
+fi
 
 echo " "
 echo "Completed resize operation"
+touch "$TOUCHFILE"
