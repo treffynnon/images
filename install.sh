a10f7e0c5d49e29f5b685dbb9a20c6f28fc45dd6
Simon Holywell <SimonHolywell@aurion.com>
2016-09-02 09:54:21 +1000

    amend install and resize so it can work on mac os too

--- install.sh
+++ install.sh
@@ -1,30 +1,44 @@
+#!/usr/bin/env bash
+
+REPO="sudo apt-get"
+if [[ $OSTYPE == darwin* ]]; then 
+    REPO="brew"
+fi
+
 echo "Installing imagemagick"
-sudo apt-get install imagemagick
+$REPO install imagemagick
 
 echo " "
 echo "Installing optipng and advdef"
-sudo apt-get install optipng advancecomp
+$REPO install optipng advancecomp
 
 echo " "
 echo "Installing gnu parallel"
-sudo apt-get install parallel
+$REPO install parallel
 
-echo " "
-echo "Installing mozjpeg"
-sudo apt-get install build-essential autoconf pkg-config nasm libtool
-git clone https://github.com/mozilla/mozjpeg.git
-cd mozjpeg
-autoreconf -fiv
-./configure --with-jpeg8
-make
-sudo make install
+if [[ $OSTYPE == darwin* ]]; then
+    echo " "
+    echo "Installing mozjpeg and jpeg-archive"
+    $REPO install mozjpeg jpeg-archive
+else
+    echo " "
+    echo "Installing mozjpeg"
+    sudo apt-get install build-essential autoconf pkg-config nasm libtool
+    git clone https://github.com/mozilla/mozjpeg.git
+    cd mozjpeg
+    autoreconf -fiv
+    ./configure --with-jpeg8
+    make
+    sudo make install
 
-cd -
+    cd -
 
-echo " "
-echo "Installing jpeg-archive"
-git clone https://github.com/danielgtaylor/jpeg-archive.git
-cd jpeg-archive
-git checkout 2.1.1
-make
-sudo make install
+    echo " "
+    echo "Installing jpeg-archive"
+    git clone https://github.com/danielgtaylor/jpeg-archive.git
+    cd jpeg-archive
+    git checkout 2.1.1
+    make
+    sudo make install
+fi
+
--- resize.sh
+++ resize.sh
@@ -9,9 +9,9 @@ TOUCHFILE="simonholywell-images.lastrun.time"
 
 if [ ! -f "$TOUCHFILE" ]; then
     # http://stackoverflow.com/a/19812608/461813
-    LAST_COMMIT_TIMESTAMP=$(git show -s --format=%ct)
+    LAST_COMMIT_TIMESTAMP=$(git show -s --date="format:%C%y%m%d%H%M.%S" --format=%cd)
     # http://unix.stackexchange.com/a/36765/10219
-    touch -d "@$LAST_COMMIT_TIMESTAMP" "$TOUCHFILE"
+    touch -t "$LAST_COMMIT_TIMESTAMP" "$TOUCHFILE"
 fi
 
 echo "Resizing in post images"
@@ -19,27 +19,27 @@ FILES=$(find src -newer "$TOUCHFILE" -iname '*.jpg' -or -newer "$TOUCHFILE" -ina
 echo "$FILES"
 if [ -n "$FILES" ]; then
     # process the large images
-    parallel -j8 convert "{}" -strip -resize "${LG_WIDTH}x${LG_HEIGHT}^" -gravity center -crop "${LG_WIDTH}x${LG_HEIGHT}+0+0" -filter catrom "t_post/{/}" ::: $FILES
+    parallel --will-cite -j8 convert "{}" -strip -resize "${LG_WIDTH}x${LG_HEIGHT}^" -gravity center -crop "${LG_WIDTH}x${LG_HEIGHT}+0+0" -filter catrom "t_post/{/}" ::: $FILES
 
     # process the image slices
-    parallel -j8 convert "t_post/{/}" -gravity center -crop "${TH_WIDTH}x${TH_HEIGHT}+0+0" -filter catrom -extent "${TH_WIDTH}x${TH_HEIGHT}" +repage "t_list/{/}" ::: $FILES
+    parallel --will-cite -j8 convert "t_post/{/}" -gravity center -crop "${TH_WIDTH}x${TH_HEIGHT}+0+0" -filter catrom -extent "${TH_WIDTH}x${TH_HEIGHT}" +repage "t_list/{/}" ::: $FILES
 fi
 
 # compress jpg images
 JPOST_FILES=$(find t_post -newer "$TOUCHFILE" -iname '*.jpg')
 JLIST_FILES=$(find t_list -newer "$TOUCHFILE" -iname '*.jpg')
 if [ -n "$JPOST_FILES" ]; then
-    parallel -j8 jpeg-recompress --method smallfry --quality medium --min 60 "{}" "{}" ::: $JPOST_FILES
+    parallel --will-cite -j8 jpeg-recompress --method smallfry --quality medium --min 60 "{}" "{}" ::: $JPOST_FILES
 fi
 if [ -n "$JLIST_FILES" ]; then
-    parallel -j8 jpeg-recompress --method smallfry --quality low --min 50 "{}" "{}" ::: $JLIST_FILES
+    parallel --will-cite -j8 jpeg-recompress --method smallfry --quality low --min 50 "{}" "{}" ::: $JLIST_FILES
 fi
 
 # compress png images
 PNG_FILES=$(find t_post t_list -newer "$TOUCHFILE" -iname '*.png')
 if [ -n "$PNG_FILES" ]; then
-    parallel -j8 optipng -o 3 -fix "{}" -out "{}" ::: $PNG_FILES
-    parallel -j8 advdef --shrink-extra -z "{}" ::: $PNG_FILES
+    parallel --will-cite -j8 optipng -o 3 -fix "{}" -out "{}" ::: $PNG_FILES
+    parallel --will-cite -j8 advdef --shrink-extra -z "{}" ::: $PNG_FILES
 fi
 
 echo " "
