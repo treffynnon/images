aa92e9975ece93d189249735d0aaf75f318060ca
Simon Holywell <treffynnon@php.net>
2017-03-03 14:15:28 +1000

    change to gulp

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const gulp = require('gulp');
+const parallel = require('concurrent-transform');
+const os = require('os');
+const changed = require('gulp-changed');
+const imageResize = require('gulp-image-resize');
+const imageMin = require('gulp-imagemin');
+const png = require('imagemin-pngquant');
+const jpeg = require('imagemin-jpeg-recompress');
+
+gulp.task('resize', function () {
+  return gulp.src('src/*')
+    .pipe(changed('test_post'))
+    .pipe(resize({
+      width: 720,
+      height: 480
+    }))
+    .pipe(moderateImageMin())
+    .pipe(gulp.dest('test_post'))
+    .pipe(resize({
+      width: 720,
+      height: 70
+    }))
+    .pipe(harshImageMin())
+    .pipe(gulp.dest('test_list'));
+});
+
+function moderateImageMin() {
+  return parallel(
+    imageMin([
+      imageMin.gifsicle(),
+      jpeg({ method: 'smallfry', min: 60 }),
+      png(),
+      imageMin.optipng(),
+      imageMin.svgo()
+    ]),
+    os.cpus().length
+  );
+}
+
+function harshImageMin() {
+  return parallel(
+    imageMin([
+      imageMin.gifsicle(),
+      jpeg({ quality: 'low', method: 'smallfry', min: 50 }),
+      png(),
+      imageMin.optipng(),
+      imageMin.svgo()
+    ]),
+    os.cpus().length
+  );
+}
+
+function resize(options) {
+  return parallel(
+    imageResize(Object.assign({
+      gravity: 'Center',
+      filter: 'catrom',
+      crop: true,
+      imageMagick: true
+    }, options)),
+    os.cpus().length
+  );
+}
